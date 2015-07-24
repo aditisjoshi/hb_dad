3c3eeddfd89c13bdd8c8e0fb25df62098905f54c
Aditi Joshi <ajoshi@IWK01MB022.internal.ngpvan.net>
2015-07-24 14:26:16 -0400

    adding javascript

---
+++ JS/javascript.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+    $("#showd").click(function(){
+        $("#decisiv").show();
+    });
+
+    $("#showc").click(function(){
+        $("#cisco").show();
+    });
+
+    $("#showp").click(function(){
+        $("#pegasus").show();
+    });
+
+    $("#showt").click(function(){
+        $("#tata").show();
+    });
+
+});
