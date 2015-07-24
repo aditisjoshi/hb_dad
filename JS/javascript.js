b2b032422ceffd9b93a0a7618610b18cc789f6f1
Aditi Joshi <ajoshi@IWK01MB022.internal.ngpvan.net>
2015-07-24 14:29:39 -0400

    fixing broken links

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,18 +1,30 @@
 $( document ).ready(function() {
     $("#showd").click(function(){
         $("#decisiv").show();
+        $("#cisco").hide();
+        $("#pegasus").hide();
+        $("#tata").hide();
     });
 
     $("#showc").click(function(){
         $("#cisco").show();
+        $("#decisiv").hide();
+        $("#pegasus").hide();
+        $("#tata").hide();
     });
 
     $("#showp").click(function(){
         $("#pegasus").show();
+        $("#cisco").hide();
+        $("#decisiv").hide();
+        $("#tata").hide();
     });
 
     $("#showt").click(function(){
         $("#tata").show();
+        $("#cisco").hide();
+        $("#pegasus").hide();
+        $("#decisiv").hide();
     });
 
 });
